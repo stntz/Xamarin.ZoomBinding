dc4dadb0a19ae657df2576b0ad4ef48f6a26c0f5
Adam Diament <adam@clevertree.io>
2021-11-07 17:59:35 +0200

    Initial upgrade attempt

---
+++ src/CommonLib/Additions/TouchImageView.partial.cs
@@ -0,0 +1,18 @@
+﻿
+// ReSharper disable once CheckNamespace this has to be this namespace
+namespace US.Zoom.Androidlib.Widget
+{
+    public partial class TouchImageView
+    {
+        void ITouchViewControl.Left()
+        {
+            InvokeLeft();
+        }
+
+        void ITouchViewControl.Right()
+        {
+            InvokeRight();
+        }
+
+    }
+}
