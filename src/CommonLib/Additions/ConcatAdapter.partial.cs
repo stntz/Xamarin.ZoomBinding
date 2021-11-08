9c94a569025ed28a96619ab3444c9f8010ccd7b9
Adam Diament <adam@clevertree.io>
2021-11-08 11:59:56 +0200

    Try completely removing concat adapter

---
+++ src/CommonLib/Additions/ConcatAdapter.partial.cs
@@ -0,0 +1,30 @@
+﻿
+//// ReSharper disable once CheckNamespace this has to be this namespace
+
+//using Java.Lang;
+
+//namespace US.Zoom.Androidlib.RecyclerView
+//{
+//    public partial class ConcatAdapter
+//    {
+//        public override void OnBindViewHolder(Object p0, int p1)
+//        {
+//            throw new System.NotImplementedException("Adam this is the concat adapter problem");
+//        }
+
+//    }
+
+//}
+
+//namespace US.Zoom.Androidlib.RecyclerView
+//{
+//    public partial class RecyclerView 
+//    {
+//        public abstract partial class Adapter : Java.Lang.Object
+//        {
+//            public abstract global::Java.Lang.Void OnBindViewHolder(
+//                global::US.Zoom.Androidlib.RecyclerView.RecyclerView.ViewHolder holder, int position);
+//        }
+//    }
+
+//}
