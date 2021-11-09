ace78e6c6eb6083f15c926a34e5a5f25c131a471
Adam Diament <adam@clevertree.io>
2021-11-09 14:41:13 +0200

    US.Google interface fixes

--- src/MobileRTC_Droid/Additions/US.Google.Protobuf.partials.cs
+++ src/MobileRTC_Droid/Additions/US.Google.Protobuf.partials.cs
@@ -4,8 +4,46 @@ using System.Text;
 
 namespace US.Google.Protobuf
 {
-    public abstract partial class ByteString
+    public sealed partial class Internal
     {
+        internal partial class IFloatListInvoker
+        {
+            IProtobufList IProtobufList.MutableCopyWithCapacity(int p0)
+            {
+                return MutableCopyWithCapacity(p0);
+            }
+        }
 
+        internal partial class ILongListInvoker
+        {
+            IProtobufList IProtobufList.MutableCopyWithCapacity(int p0)
+            {
+                return MutableCopyWithCapacity(p0);
+            }
+        }
+
+        internal partial class IDoubleListInvoker
+        {
+            IProtobufList IProtobufList.MutableCopyWithCapacity(int p0)
+            {
+                return MutableCopyWithCapacity(p0);
+            }
+        }
+
+        internal partial class IBooleanListInvoker
+        {
+            IProtobufList IProtobufList.MutableCopyWithCapacity(int p0)
+            {
+                return MutableCopyWithCapacity(p0);
+            }
+        }
+
+        internal partial class IIntListInvoker
+        {
+            IProtobufList IProtobufList.MutableCopyWithCapacity(int p0)
+            {
+                return MutableCopyWithCapacity(p0);
+            }
+        }
     }
 }
