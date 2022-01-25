20bb715a2db2280942ee006d132ac9852ccba10a
Adam Diament <adam@clevertree.io>
2022-01-25 15:53:23 +0200

    More metadata fixes for commonlib - 10 errors left

---
+++ src/CommonLib/Additions/ZmBaseNodeAdapter.extend.cs
@@ -0,0 +1,13 @@
+﻿using Object = Java.Lang.Object;
+
+// ReSharper disable once CheckNamespace
+namespace US.Zoom.Androidlib.Widget.Recyclerview.Provider.Node
+{
+    public partial class ZMBaseNodeAdapter
+    {
+        protected override void Convert(Java.Lang.Object p0, Object p1)
+        {
+            Convert((ZMBaseRecyclerViewItemHolder)p0,p1);
+        }
+    }
+}
